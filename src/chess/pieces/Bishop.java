178b744b6a26eac3ea0fbb791e5c3fb437421079
Max Santos <max.well.guerreiro12@gmail.com>
2026-01-29 10:23:21 -0300

    Add bishop

---
+++ src/chess/pieces/Bishop.java
@@ -0,0 +1,68 @@
+package chess.pieces;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Bishop extends ChessPiece {
+
+	public Bishop(Board board, Color color) {
+		super(board, color);
+	}
+
+	@Override
+	public String toString() {
+		return "B";
+	}
+
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+		Position p = new Position(0, 0);
+
+		// nw
+		p.setValues(position.getRow() - 1, position.getcolumn() -1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getcolumn()] = true;
+			p.setValues(p.getRow() - 1, p.getcolumn() - 1);
+		}
+		if (getBoard().positionExists(p) && isThereOpenentPiece(p)) {
+			mat[p.getRow()][p.getcolumn()] = true;
+		}
+
+		// ne
+		p.setValues(position.getRow() -1, position.getcolumn() + 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getcolumn()] = true;
+			p.setValues(p.getRow() - 1, p.getcolumn() +1);
+		}
+		if (getBoard().positionExists(p) && isThereOpenentPiece(p)) {
+			mat[p.getRow()][p.getcolumn()] = true;
+		}
+
+		// se
+		p.setValues(position.getRow() +1, position.getcolumn() + 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getcolumn()] = true;
+			p.setValues(p.getRow() +1, p.getcolumn() +1);
+		}
+		if (getBoard().positionExists(p) && isThereOpenentPiece(p)) {
+			mat[p.getRow()][p.getcolumn()] = true;
+		}
+
+		// sw
+		p.setValues(position.getRow() + 1, position.getcolumn() - 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getcolumn()] = true;
+			p.setValues(p.getRow() + 1, p.getcolumn() -1);;
+		}
+		if (getBoard().positionExists(p) && isThereOpenentPiece(p)) {
+			mat[p.getRow()][p.getcolumn()] = true;
+		}
+
+		return mat;
+	}
+
+}
